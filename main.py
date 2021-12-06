ddbdc411614175ba8ed4fba93d87813e0ea08c86
Лесс МА <BearForest2006@gmail.com>
2021-12-06 18:20:20 +0300

    создали главный код

---
+++ main.py
@@ -0,0 +1,14 @@
+def my_super_function():
+    print("what an awesome print!")
+
+
+def main():
+    print("My first git program")
+    print("And I change it every day")
+    print("Again")
+    print("UFO came and added this line")
+    my_super_function()
+
+
+if __name__ == "__main__":
+    main()
